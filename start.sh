27d36ac5bbc0252263f890b92646ac4d04d05706
abbachinomnso <abbachinomnso@gmail.com>
2017-12-08 14:30:17 +0000

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server eu.pool.gold --user GgTiJQgAu8S9QhP9TRujGeBRDpF8JLdMfB.abba --pass x --port 3044
+./miner --server eu.pool.gold --user GgTiJQgAu8S9QhP9TRujGeBRDpF8JLdMfB.abba2 --pass x --port 3044
